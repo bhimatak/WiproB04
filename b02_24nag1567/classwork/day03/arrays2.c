6ef40369741c9c076b05a7f028d5c68d4e1ff8e0
bhimatak <bhima.t@gmail.com>
2024-06-27 16:51:59 +0530

    updated

--- b02_24nag1567/classwork/day03/arrays2.c
+++ b02_24nag1567/classwork/day03/arrays2.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
-#define CAP 5
+#define CAP 10
 
 int main()
 {
-    int arr[CAP];
-    int noElement=0;
-    printf("\nSize of the array in bytes: %d",sizeof(arr));
-    noElement = sizeof(arr)/sizeof(arr[0]);
-    printf("\nNo of elements : %d",noElement);
+    int arr[CAP], i, sum=0, t;
+    float avg=0.0;
+    printf("\nEnter the elements of the array\n");
+    for(i=0;i<CAP;i++)
+        scanf("%d",&arr[i]);
+    
+    /*
+    for(i=0;i<CAP;i++){
+        arr[i] *=2;
+        sum +=arr[i];
+    }
+    */
+    for(i=0;i<CAP/2;i++)
+    {
+        t = arr[i];
+        arr[i] = arr[CAP-1-i];
+        arr[CAP-1-i]=t;
+    }
+    /*
+    i=0
+    arr[0] 5, arr[4] = 1
+    i=1
+    arr[1]=4, arr[3] = 2
+    i=2
+    t = arr[2] =3
+    arr[2] = 3;
+    arr[2]=t=3
+    i=3
+    t = arr[3]=2
+    arr[3] =  4
+    arr[1] = 2
+
+    arr[0]5
+    arr[1]=2
+    arr[2]=3
+    arr[3]=4
+    arr[4]=1
+    */
+    printf("\nElement of the array are\n");
+    for(i=0;i<CAP;i++)
+        printf("%d ",arr[i]);
+    // avg = (float)sum/CAP;
+    // printf("\nAvg: = %f",avg);    
 
     printf("\n\n");
 
