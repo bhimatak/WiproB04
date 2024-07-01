e7cef41a5f289de07cda45c602d1368289014482
b129user01 <b129user01@rpsconsulting.in>
2024-07-01 16:51:00 +0530

    updated

--- b02_24nag1567/classwork/day05/prg10.c
+++ b02_24nag1567/classwork/day05/prg10.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int a=10;
+    int *ptr;
+    // ptr = &a;
+    //ptr = address
+    ptr = (int *)malloc(sizeof(int));
+
+    printf("\nptr's address = %u",&ptr);
+    printf("\nAddress pointed/assigned to ptr = %u",ptr);
+    *ptr = 101;
+    printf("\nValue %d is stored at %u address", *ptr, ptr);
+
+    printf("\n%d %d",*ptr,a);
+    printf("\n\n");
+
+    return 0;
+}
