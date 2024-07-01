915a9ac3c909181864d4a577958fb1ad504d1caa
b129user01 <b129user01@rpsconsulting.in>
2024-07-01 17:12:28 +0530

    updated

--- b02_24nag1567/classwork/day05/prg10.c
+++ b02_24nag1567/classwork/day05/prg10.c
@@ -6,14 +6,18 @@ int main()
 {
     int a=10;
     int *ptr;
+    int *temp;
     // ptr = &a;
     //ptr = address
     ptr = (int *)malloc(sizeof(int));
+    temp = ptr;
 
     printf("\nptr's address = %u",&ptr);
+    printf("\ntemp's address = %u",&temp);
     printf("\nAddress pointed/assigned to ptr = %u",ptr);
     *ptr = 101;
     printf("\nValue %d is stored at %u address", *ptr, ptr);
+    printf("\nValue %d is stored at %u address", *temp, temp);
 
     printf("\n%d %d",*ptr,a);
     printf("\n\n");
---
+++ b02_24nag1567/classwork/day05/prg11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    // int a[20];
+    int i, sum=0;
+    int *ptr, *temp;
+    float avg;
+    int noOfElem = 5;
+
+    // ptr = a;
+    ptr  = (int *) malloc(noOfElem *sizeof(int));
+    temp = ptr;
+    for(sum=0,i=0;i<noOfElem;i++,ptr++){
+        scanf("%d",ptr);
+        sum += *ptr;
+    }
+
+    avg = (float)sum/noOfElem;
+    ptr = temp;
+    for(i=0;i<noOfElem;i++, ptr++)
+        printf(" %d",*ptr);
+    printf("\nAvg = %f",avg);
+
+    printf("\n\n");
+
+    return 0;
+    
+}
