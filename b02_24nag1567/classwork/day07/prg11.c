2577d64bb6897a83241c79127902d1153b76940c
b129user01 <b129user01@rpsconsulting.in>
2024-07-03 17:19:30 +0530

    updated

--- b02_24nag1567/classwork/day07/prg11.c
+++ b02_24nag1567/classwork/day07/prg11.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "emp.h"
 #include "emp.c"
 
 int main()
 {
     EMP e;
+    char line[1024];
+    char *tokens;
     FILE *fptr;
     fptr = fopen("emp.data","r");
-    fscanf(fptr,"%s %d %d",e.empName, &e.empId, &e.empSal);
+    // fscanf(fptr,"%s %d %d",e.empName, &e.empId, &e.empSal);
+    fgets(line,1024,fptr);
+    
+    printf("\n%s",line);
 
+    tokens = strtok(line," ");
+    strcpy(e.empName, tokens);
+    tokens = strtok(NULL, " ");
+    e.empId = atoi(tokens);
+    tokens = strtok(NULL, " ");
+    e.empSal = atoi(tokens);
+     
+    
     display(e);
     
     printf("\n\n");
